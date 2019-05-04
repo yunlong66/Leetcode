6831753892c113f0ac4fe44bf7b2819b2d367633
Yan Yunlong <18810208912@163.com>
2019-05-04 16:04:56 +0800

    clean

--- src/f1.java
+++
@@ -1,6 +0,0 @@
-public class f1 {
-    private int i = 0;
-    public static void main(String[] args){
-
-    }
-}
--- src/f2.java
+++
@@ -1,7 +0,0 @@
-public class f2 {
-    public int i = 1;
-    public int j = 1;
-    public static void main(String[] args){
-
-    }
-}
